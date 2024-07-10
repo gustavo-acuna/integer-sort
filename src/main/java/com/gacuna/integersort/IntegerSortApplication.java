5a5134a091fc918b409ffa7375e5ed5e4c6ffba2
Gustavo Acuña <gustavojose.acuna.contractor@bbva.com>
2024-07-10 15:44:16 +0200

    initial commit

---
+++ src/main/java/com/gacuna/integersort/IntegerSortApplication.java
@@ -0,0 +1,13 @@
+package com.gacuna.integersort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegerSortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gacuna/integersort/IntegerSortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gacuna.integersort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegerSortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
